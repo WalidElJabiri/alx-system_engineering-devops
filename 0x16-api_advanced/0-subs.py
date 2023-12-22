18a1a37825b48f7a91d05b39978e39fb77c65ff1
walid <jabiriwalid555@gmail.com>
2023-12-23 00:42:07 +0100

    adding tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,7 @@ BASE_URL = 'http://reddit.com/r/{}/about.json'
 def number_of_subscribers(subreddit):
     '''Get number of subscribers'''
     headers = {'User-agent': 'Unix:0-subs:v1'}
-    response = requests.get(BASE_URL.format(subreddit),
-                        ders=headers)
+    response = requests.get(BASE_URL.format(subreddit), ders=headers)
     if response.status_code != 200:
         return 0
     return response.json().get('data', {}).get('subscribers', 0)
